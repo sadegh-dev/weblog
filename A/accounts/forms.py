c6b58ce2d3bb016bd00a4b83089aeafb1739d04e
charlie <sadegh.rez@gmail.com>
2021-01-31 20:10:50 +0330

    create messages

--- A/accounts/forms.py
+++ A/accounts/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(max_length=30)
+    
--- A/accounts/views.py
+++ A/accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserLoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def user_login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username= form.cleaned_data['username']
+            password= form.cleaned_data['password']
+            user = authenticate(request, username= username, password= password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully !','success')
+                return redirect('blog:all_articles') 
+            else :
+                messages.error(request, 'wrong yousername or password !','warning')          
+    else:
+        form = UserLoginForm()
+    
     context = {'form': form}
     return render(request, 'accounts/login.html', context)
