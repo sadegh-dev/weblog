00711ede8228630c4c1cff427c2743c6970bb284
charlie <sadegh.rez@gmail.com>
2021-02-01 23:14:18 +0330

    develop admin panel -Article page

--- A/blog/admin.py
+++ A/blog/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Articles
 
-admin.site.register(Articles)
+
+@admin.register(Articles)           #way1
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'writer', 'publish', 'status')
+    list_filter = ('status', 'writer', 'publish')
+    list_editable = ('status',)
+    search_fields = ('title', 'body')
+    raw_id_fields = ('writer',)
+    prepopulated_fields = {'slug':('title',)}
+
+#admin.site.register(Articles)      #way2
 
 
 
