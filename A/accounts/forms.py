1c4268982046492372c31e9c0f866b7df9671457
charlie <sadegh.rez@gmail.com>
2021-02-01 18:24:24 +0330

    overriding clean method in forms.py

--- A/accounts/forms.py
+++ A/accounts/forms.py
@@ -9,7 +9,8 @@ class UserLoginForm(forms.Form):
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'your username'}))
     email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'your email'}))
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password = forms.CharField(label='password', max_length=30, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password2 = forms.CharField(label='confirm password', max_length=30, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -17,5 +18,27 @@ class UserRegistrationForm(forms.Form):
         if user.exists():
             raise forms.ValidationError('this email already exists')
         return email
+    
+    # Way 1 : password
+    """
+    def clean_password2(self):
+        p = self.cleaned_data['password']
+        p2 = self.cleaned_data['password2']
+        if p != p2 :
+            raise forms.ValidationError('passwords must match')
+        return p
+    """
+
+    # Way 2 : password
+    def clean(self):
+        cleaned_data = super().clean()
+        p = cleaned_data.get('password')
+        p2 = cleaned_data.get('password2')
+        if p and p2 :
+            if p != p2 :
+                raise forms.ValidationError('passwords must match')
+
+        
+
 
 
